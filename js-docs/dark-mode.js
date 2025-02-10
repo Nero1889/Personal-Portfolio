15821098e3632d3cdffa5cc082cb229bed79c367
Raul Luna <mceclipse1889@gmail.com>
2025-02-09 19:35:40 -0600

    Update dark-mode.js
    (Ternary operator) Further optimized theme toggle!

--- js-docs/dark-mode.js
+++ js-docs/dark-mode.js
@@ -1,6 +1,6 @@
 const THEME_TOGGLE_BTNS = document.querySelectorAll(".theme-toggle");
-
 const CURRENT_THEME = localStorage.getItem("theme");
+
 if (CURRENT_THEME === "light") {
     document.body.classList.add("light-mode");
 }
@@ -9,12 +9,10 @@ THEME_TOGGLE_BTNS.forEach(btn => {
     btn.addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
 
-        if (document.body.classList.contains("light-mode")) {
-            localStorage.setItem("theme", "light");
-        } else {
-            localStorage.removeItem("theme", "dark");
-        }
-
+        document.body.classList.contains("light-mode") 
+        ? localStorage.setItem("theme", "light") 
+        : localStorage.removeItem("theme");
+        
         document.body.style.transition = "background-color .2s ease-in-out";
     });
 }); 
