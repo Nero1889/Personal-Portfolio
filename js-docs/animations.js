41669200f17ccea55c497cd9105ebb0007f6458b
Raul Luna <mceclipse1889@gmail.com>
2025-05-04 23:16:29 -0500

    Update animations.js

--- js-docs/animations.js
+++ js-docs/animations.js
@@ -1,3 +1,25 @@
+/* Scaatter and Gather Effect */
+document.addEventListener("DOMContentLoaded", () => {
+    const HERO_TITLE = document.getElementById("hero-title");
+    const LETTERS = HERO_TITLE.querySelectorAll("span");
+  
+    LETTERS.forEach((letter) => {
+      const X = (Math.random() - 0.5) * 40;
+      const Y = (Math.random() - 0.5) * 40;
+      const ROTATION = (Math.random() - 0.5) * 360;
+      const DELAY = Math.random() * 325;
+  
+      letter.style.transform = `translate(${X}vw, ${Y}vh) rotate(${ROTATION}deg)`;
+      letter.style.opacity = 1;
+      letter.style.animation = `scatterAndGather .75s forwards ${DELAY}ms`;
+  
+      letter.dataset.initialTransform = `translate(0px, 0px) rotate(0deg)`;
+      letter.dataset.x = 0;
+      letter.dataset.y = 0;
+    });
+});
+
+/* Lazy Loading Effect for Images */
 const LAZY_IMG = document.querySelectorAll(".lazy");
 const OBSERVER = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
@@ -11,4 +33,4 @@ const OBSERVER = new IntersectionObserver((entries, observer) => {
     });
 });
     
-LAZY_IMG.forEach((img) => OBSERVER.observe(img));
+LAZY_IMG.forEach((img) => OBSERVER.observe(img)); 
