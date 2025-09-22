d33e4cfe3bc0aa42e1fa30881afe08c3313fa62e
Raul Luna <mceclipse1889@gmail.com>
2025-09-22 02:06:29 -0500

    Update main.js

--- js-docs/main.js
+++ js-docs/main.js
@@ -1,6 +1,8 @@
 const HEADER_BARS = document.querySelector("#header-bars");
 const MOBILE_NAV = document.querySelector("#mobile-nav");
 const MOBILE_LINKS = document.querySelectorAll(".mobile-link");
+const MENU_ICON = "images/menu.png";
+const CLOSE_ICON = "images/close.png";
 let isMobileNavOpen = false;
     
 HEADER_BARS.addEventListener("click", () => {
@@ -21,5 +23,27 @@ MOBILE_LINKS.forEach(link => {
 
         MOBILE_NAV.style.display = "none";
         document.body.style.overflowY = "auto";
+    });
+});
+
+
+HEADER_BARS.addEventListener("click", () => {
+    if (MOBILE_NAV.style.display === "none" || MOBILE_NAV.style.display === "") {
+        MOBILE_NAV.style.display = "flex";
+        HEADER_BARS.src = CLOSE_ICON;
+        HEADER_BARS.alt = "Close Icon";
+    } else {
+        MOBILE_NAV.style.display = "none";
+        HEADER_BARS.src = MENU_ICON;
+        HEADER_BARS.alt = "Menu Icon";
+    }
+});
+
+const mobileLinks = document.querySelectorAll(".mobile-link");
+mobileLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        MOBILE_NAV.style.display = "none";
+        HEADER_BARS.src = MENU_ICON;
+        HEADER_BARS.alt = "Menu Icon";
     });
 });
